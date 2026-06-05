c48638af29f33b1878a529543e247cac17918281
Dan Carter <djcarter85@gmail.com>
2026-06-05 22:37:33 +0100

    feat: add email address to navbar

--- src/layouts/loggedInLayout.tsx
+++ src/layouts/loggedInLayout.tsx
@@ -1,20 +1,30 @@
-import { Link, Outlet } from "react-router";
+import { useAuth } from "react-oidc-context";
+import { Link, Outlet, useNavigate } from "react-router";
 
-const Nav = () => {
+const Nav = ({ user }: { user: string | undefined }) => {
   return (
     <nav>
       <Link to="/leaderboard">Leaderboard</Link>
       <Link to="/predictions">Predictions</Link>
+      <div>Logged in as: {user}</div>
     </nav>
   );
 };
 
 export const LoggedInLayout = () => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  if (!auth.isAuthenticated) {
+    navigate("/");
+    return null;
+  }
+
   return (
     <div>
       <h1>Logged In Layout</h1>
       <p>This is the layout for logged in users.</p>
-      <Nav />
+      <Nav user={auth.user?.profile.email} />
       <Outlet />
     </div>
   );
