d4c82db232b8bc8d6cfae0c3be62c45bbb27ed15
Dan Carter <djcarter85@gmail.com>
2026-06-05 20:22:31 +0100

    feat: add basic auth integration with existing Cognito user pool

--- src/main.tsx
+++ src/main.tsx
@@ -7,20 +7,31 @@ import { PredictionsPage } from "./pages/predictionsPage";
 import { LoginPage } from "./pages/loginPage";
 import { LoggedInLayout } from "./layouts/loggedInLayout";
 import { NotFoundPage } from "./pages/notFoundPage";
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.eu-west-2.amazonaws.com/eu-west-2_JwWpg0O8l",
+  client_id: "2egge4ro1oduqes2bn2q4snrah",
+  redirect_uri: `${window.location.origin}/`,
+  response_type: "code",
+  scope: "email openid phone",
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
+    <AuthProvider {...cognitoAuthConfig}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
 
-        <Route element={<LoggedInLayout />}>
-          <Route path="/predictions" element={<PredictionsPage />} />
-          <Route path="/leaderboard" element={<LeaderboardPage />} />
-        </Route>
+          <Route element={<LoggedInLayout />}>
+            <Route path="/predictions" element={<PredictionsPage />} />
+            <Route path="/leaderboard" element={<LeaderboardPage />} />
+          </Route>
 
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>,
 );
--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,8 +1,28 @@
+import { useAuth } from "react-oidc-context";
+import { useNavigate } from "react-router";
+
 export const LoginPage = () => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    navigate("/leaderboard");
+    return null;
+  }
+
   return (
     <div>
       <h1>Login</h1>
       <p>This is the login page.</p>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
     </div>
   );
 };
