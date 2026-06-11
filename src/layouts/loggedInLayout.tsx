335ea18b95e75b86c6caaf24623469ea776fb20d
Dan Carter <djcarter85@gmail.com>
2026-06-11 14:38:52 +0100

    feat: remove nav

--- src/layouts/loggedInLayout.tsx
+++ src/layouts/loggedInLayout.tsx
@@ -1,14 +1,5 @@
 import { useAuth } from "react-oidc-context";
-import { Link, Outlet, useNavigate } from "react-router";
-
-const Nav = ({ user }: { user: string | undefined }) => {
-  return (
-    <nav>
-      <Link to="/leaderboard">Leaderboard</Link>
-      <div>Logged in as: {user}</div>
-    </nav>
-  );
-};
+import { Outlet, useNavigate } from "react-router";
 
 export const LoggedInLayout = () => {
   const auth = useAuth();
@@ -21,7 +12,6 @@ export const LoggedInLayout = () => {
 
   return (
     <div>
-      <Nav user={auth.user?.profile.email} />
       <Outlet />
     </div>
   );
