fb8ac7007609d7580e405f1297f38e24c51718a1
Dan Carter <djcarter85@gmail.com>
2026-06-15 09:49:07 +0100

    feat: add some styling to the login page

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -21,10 +21,19 @@ export const LoginPage = () => {
 
   return (
     <Main>
-      <div>
-        <h1>Login</h1>
-        <p>This is the login page.</p>
-        <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="mx-auto flex w-xs flex-col gap-y-4 rounded-xl border border-gray-300 p-6 shadow-md">
+          <p className="text-center text-lg">
+            Welcome to PTO, a football predictions game!
+          </p>
+          <p className="text-center text-sm">You must be logged in to play.</p>
+          <button
+            onClick={() => auth.signinRedirect()}
+            className="w-full rounded-lg bg-blue-500 px-3 py-2 text-white hover:bg-blue-600"
+          >
+            Log in or sign up
+          </button>
+        </div>
       </div>
     </Main>
   );
